1c96769700be0cd87bc708164880f4b1a22eb49d
Bautista Berto <62818001+BertovDev@users.noreply.github.com>
2022-10-28 15:19:29 +0000

    reto8 completed

--- reto8.js
+++ reto8.js
@@ -1,6 +1,24 @@
 function maxProfit(prices) {
-  // ¡Y no olvides compartir tu solución en redes!
-  return false
+  let resMinMax = 0;
+
+  console.log(prices)
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (i === 0 && j === i + 1) {
+        resMinMax = prices[j] - prices[i]
+      }
+      if (resMinMax < (prices[j] - prices[i])) {
+        resMinMax = prices[j] - prices[i]
+      }
+    }
+
+  }
+
+  if (resMinMax === 0) resMinMax = -1;
+  console.log(resMinMax)
+
+  return resMinMax
 }
 
 const pricesBtc = [39, 18, 29, 25, 34, 32, 5]
